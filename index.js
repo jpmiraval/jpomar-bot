66ed8902b0eb638f1d86402be1ce82b6d5c087b9
omarchelo2111 <51802105+omarchelo2111@users.noreply.github.com>
2021-05-22 17:55:19 -0500

    Updating index.js, adding const config

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const config = require('./config.json');
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
@@ -6,18 +7,18 @@ client.once('ready', () => {
 });
 
 client.on('message', message => {
-	if (message.content === '!horarios') {
-		message.channel.send('Pingaloca');
-	}
-	
-	else if (message.content === '!omar') {
-		message.channel.send('OMARPINGA');
-	}
-	
-	else if (message.content === '!orkito') {
-		message.channel.send('ROKOTOOOOOOOOOOOOOOOOOO');
-	}
-	
+    if (message.content === '!horarios') {
+        message.channel.send('Pingaloca');
+    }
+
+    else if (message.content === '!omar') {
+        message.channel.send('OMARPINGA');
+    }
+
+    else if (message.content === '!orkito') {
+        message.channel.send('ROKOTOOOOOOOOOOOOOOOOOO');
+    }
+
 });
 
-client.login("ODQ1MzU0MjM3MjE1MjQ0Mjk4.YKfvfA.jnU1Vuz0e9MAcVkSkzyqjtQI8XA");
+client.login(config.token);
